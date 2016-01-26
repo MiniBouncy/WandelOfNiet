a6ae5efccb107f4ef9b0fc2fd7d5e890e0acc4e1
TranceCake <info@niekgigengack.nl>
2016-01-26 21:01:41 +0100

    dingen

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../db/models/user');
+var querystring = require('querystring');
+var http = require('http');
 
 
 router.route('/')
@@ -187,36 +189,89 @@ router.route('/:user_id/balance')
 
     }
   })
-})
+});
 
-// Updates a users' balance POST-PUT-GET
+router.route('/transaction')
+
 // TODO bank stuurt true terug als het aan die kant geslaagd is en dan kan de balance worden veranderd.
-.put(function(req, res) {
+.post(function(req, res, next) {
+  var salt = 'A58JFK9874LAK';
+  var hash = null; //hash('sha256', 'wandelOfNiet' + salt);
+
+  var postData = querystring.stringify({
+    'sender' : 'NL16RABO0846653421',
+    'receiver' : 'NL08RABO0784598758',
+    'description' : 'bla',
+    'amount' : 5,
+    'hash' : hash
+  });
+
+  var options = {
+    hostname: 'localhost',
+    port: 80,
+    path: '/bank/payment',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': postData.length
+    }
+  };
+
+  var req = http.request(options, (res) => {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log('BODY: ' + chunk);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.')
+    })
+  });
+
+  req.on('error', (e) => {
+    console.log('problem with request: ' + e.message);
+  });
+
+  // write data to request body
+  req.write(postData);
+  req.end();
+
+});
+
+  // var sender = "NL16RABO0846653421";
+  // var receiver = "NL08RABO0784598758";
+  // var description = "bla";
+  // var amount = 5;
+  // var request = require('sync-request');
+  // var response = request('POST', 'http://localhost/bank/payment?sender=' + sender + '&receiver=' + receiver + '&amount=' + amount + '&description=' + description);
+  // var body = response.getBody().toString();
+  // var obj = JSON.parse(body);  
+  // console.log('id ' +obj['id']);
+
+  // var payRes = request('PUT', 'http://localhost/bank/payment/' +obj['id']);
+  // var payBody = payRes.getBody().toString();
+  // console.log('put: ' +payBody);
+
+  // var reqBalance = request('GET', 'http://localhost/bank/payment/' +obj['id']);
+  // var bodyBalance = reqBalance.getBody().toString();
+  // console.log('get: ' +bodyBalance['amount']);
+
+  // if(req.body.balance){
+  //        user.balance = req.bodyBalance['amount'];
+  // }
+
+
+  /*
   var sender = "NL16RABO0846653421";
   var receiver = "NL08RABO0784598758";
   var description = "bla";
   var amount = 5;
+  var salt = 'A58JFK9874LAK';
+  var hash = hash('sha256', 'wandelOfNiet' + salt);
   var request = require('sync-request');
   var response = request('POST', 'http://localhost/bank/payment?sender=' + sender + '&receiver=' + receiver + '&amount=' + amount + '&description=' + description);
-  var body = response.getBody().toString();
-  var obj = JSON.parse(body);  
-  console.log('id ' +obj['id']);
-
-  var payRes = request('PUT', 'http://localhost/bank/payment/' +obj['id']);
-  var payBody = payRes.getBody().toString();
-  console.log('put: ' +payBody);
-
-  var reqBalance = request('GET', 'http://localhost/bank/payment/' +obj['id']);
-  var bodyBalance = reqBalance.getBody().toString();
-  console.log('get: ' +bodyBalance['amount']);
-
-  if(req.body.balance){
-         user.balance = req.bodyBalance['amount'];
-  }
-
-  
-
-})
+  */
 
 router.route('/login')
 
